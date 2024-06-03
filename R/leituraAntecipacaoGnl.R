af73f1d25053d0782e8bb1a8068eca35f2e0f13f
Saulo Ribeiro Silva <saulo.silva@epe.gov.br>
2024-06-03 15:23:30 -0300

    modificação para considerar arquivo adterm vazio

--- R/leituraAntecipacaoGnl.R
+++ R/leituraAntecipacaoGnl.R
@@ -24,44 +24,49 @@ leituraAntecipacaoGnl <- function(pastaCaso) {
   if (missing(pastaCaso)) {
     stop("favor indicar a pasta com os arquivos do NEWAVE")
   }
-
+  
   # encontra o nome do arquivo adterm de acordo com a ordem informada no manual do NEWAVE para o arquivos.dat
   arquivo <- leituraArquivos(pastaCaso) %>%
     dplyr::slice(32) %>%
     dplyr::pull(arquivo)
-
+  
   # verifica existencia do arquivo
   if (!file.exists(paste(pastaCaso, arquivo, sep = "/"))) {
     stop(paste0(arquivo, " n\u00E3o encontrado em ", pastaCaso))
   }
-
+  
   # le o numero de patamares do caso
   nPat <- leituraNumPat(pastaCaso)
-
+  
   # le o arquivo sistema como um vetor de caracteres
   dadosBrutos <- readr::read_lines(stringi::stri_enc_toutf8(paste(pastaCaso, arquivo, sep = "/")), locale = readr::locale(encoding = "latin1"))
-
+  
   # Encontra as colunas
   posicaoColunasInicio <- c(2, seq.int(25, 25 + 12 * (nPat - 1), 12))
   posicaoColunasFim <- c(5, seq.int(34, 34 + 12 * (nPat - 1), 12))
-
+  
   df.antecipacaoDespachoGnl <- readr::read_fwf(I(dadosBrutos[-length(dadosBrutos)]),
-    col_positions = readr::fwf_positions( # vetor com as posicoes iniciais de cada campo
-      posicaoColunasInicio,
-      # vetor com as posicoes finais de cada campo
-      posicaoColunasFim,
-      c("codUsina", seq(1, nPat))
-    ),
-    skip = 2,
-    show_col_types = FALSE
+                                               col_positions = readr::fwf_positions( # vetor com as posicoes iniciais de cada campo
+                                                 posicaoColunasInicio,
+                                                 # vetor com as posicoes finais de cada campo
+                                                 posicaoColunasFim,
+                                                 c("codUsina", seq(1, nPat))
+                                               ),
+                                               skip = 2,
+                                               show_col_types = FALSE
   ) %>%
     tidyr::fill(codUsina) %>%
     dplyr::filter(!is.na(`1`)) %>%
-    dplyr::group_by(codUsina) %>%
-    dplyr::mutate(lag = 1:dplyr::n()) %>%
-    dplyr::select(codUsina, lag, dplyr::everything()) %>%
-    tidyr::pivot_longer(3:(2 + nPat), names_to = "patamar", values_to = "despacho") %>%
-    dplyr::mutate(patamar = as.numeric(patamar))
-
+    dplyr::group_by(codUsina)
+  
+  # se não existirem dados, retorna o df vazio
+  if(nrow(df.antecipacaoDespachoGnl) > 0){
+    df.antecipacaoDespachoGnl <- df.antecipacaoDespachoGnl %>%
+      dplyr::mutate(lag = 1:dplyr::n()) %>%
+      dplyr::select(codUsina, lag, dplyr::everything()) %>%
+      tidyr::pivot_longer(3:(2 + nPat), names_to = "patamar", values_to = "despacho") %>%
+      dplyr::mutate(patamar = as.numeric(patamar))
+  }
+  
   return(df.antecipacaoDespachoGnl)
 }
